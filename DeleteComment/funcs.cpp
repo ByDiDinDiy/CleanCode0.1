af8f7a6b7a3ecbaf61e9d0abb2bd8319e46caadb
ByDiDinDiy <dmitriy0bydi@gmail.com>
2020-06-27 17:40:40 +0400

    added empty function

--- DeleteComment/funcs.cpp
+++ DeleteComment/funcs.cpp
@@ -1,6 +1,32 @@
+//#include "stdafx.h"
 #include "funcs.h"
+#include <stdio.h>
+#include <string.h>
+#include <cctype>
 
-int SomeFunc()
+bool findStrInText(const char text[MAX_SIZE_STRING][MAX_SIZE_SYMBOL], const int lengthOfText, const char* symbolInput, int* x, int* y)
+{
+	return;
+}
+
+bool cmpCoordinate(const int x1, const int y1, const int x2, const int y2)
+{
+	return;
+}
+
+int deleteCommentsInCode(char text[MAX_SIZE_STRING][MAX_SIZE_SYMBOL], const int lengthOfText)
 {
 	return 0;
 }
+
+int deletingRows(char text[MAX_SIZE_STRING][MAX_SIZE_SYMBOL], int lengthOfTextInput, const int beginStr, const int quantityStr)
+{
+	return 0;
+}
+
+int  deleteText(char text[MAX_SIZE_STRING][MAX_SIZE_SYMBOL], const int lengthOfTextInput, const int beginX, const int beginY, const int endX, const int endY)
+{
+	return 0;
+}
+
+
