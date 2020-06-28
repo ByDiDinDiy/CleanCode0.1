43f1f07173fb8e9a8dc5bcb9970720c6b480d801
ByDiDinDiy <dmitriy0bydi@gmail.com>
2020-06-28 06:27:24 +0400

    create all function

--- DeleteComment/funcs.cpp
+++ DeleteComment/funcs.cpp
@@ -1,4 +1,3 @@
-//#include "stdafx.h"
 #include "funcs.h"
 #include <stdio.h>
 #include <string.h>
@@ -63,17 +62,64 @@ bool cmpCoordinate(const int x1, const int y1, const int x2, const int y2)
 
 int deleteCommentsInCode(char text[MAX_SIZE_STRING][MAX_SIZE_SYMBOL], const int lengthOfText)
 {
-	return 0;
-}
+	int singleLine_x = -1, singleLine_y = -1;
+	int multiLineBegin_x = -1, multiLineBegin_y = -1;
+	int multiLineEnd_x = -1, multiLineEnd_y = -1;
 
-int deletingRows(char text[MAX_SIZE_STRING][MAX_SIZE_SYMBOL], int lengthOfTextInput, const int beginStr, const int quantityStr)
-{
-	return 0;
+	int countOfStrings = lengthOfText;
+
+	//Ищем первый встречающийся комментарий в тексте, и запоминаем его координаты
+	bool comment = findStrInText(text, countOfStrings, "/*", &multiLineBegin_x, &multiLineBegin_y);
+
+	//Пока в тексте встречаются комментарии
+	while (comment) {
+		//Ищем сиволы закрывающий многострочный комментарий
+		findStrInText(text, lengthOfText, "*/", &multiLineEnd_x, &multiLineEnd_y);
+		//Удаляем комментарии
+		countOfStrings = deleteText(text, countOfStrings, multiLineBegin_x, multiLineBegin_y, multiLineEnd_x + 1, multiLineEnd_y);
+
+		//Ищем первый встречающийся комментарий в тексте, и запоминаем его координаты
+		comment = findStrInText(text, countOfStrings, "/*", &multiLineBegin_x, &multiLineBegin_y);
+	}
+
+	return countOfStrings;
 }
 
 int  deleteText(char text[MAX_SIZE_STRING][MAX_SIZE_SYMBOL], const int lengthOfTextInput, const int beginX, const int beginY, const int endX, const int endY)
 {
-	return 0;
+	int lengthOfText = *const_cast<int*>(&lengthOfTextInput);
+
+	int beginStr; //первая строка для для затирания
+	int quantityStr; //количество строк для удаления 
+
+	//Если удаляемый текст находится на одной строке, то 
+	if ((beginY) == (endY)) {
+		//Удаляем текст 
+		strcpy(text[beginY] + beginX, text[beginY] + endX + 1);
+		//Если в строчке больше нет символов, то
+		if (text[beginY][0] == '\0')
+			//удаляем строчку
+			lengthOfText = deletingRows(text, lengthOfText, beginY, 1);
+
+	}
+	else {
+
+		//удалим комментарий на первой строке его вхождения 
+		strcpy(text[beginY] + beginX, "\0");
+		//если строка состояла только из комментария, то запоминаем её как начало затирания, иначе следующую 
+		(text[beginY][0] == '\0') ? beginStr = beginY : beginStr = (beginY)+1;
+
+		//удалим комментарий на последней строке его вхождения 
+		strcpy(text[endY], text[endY] + (endX)+1);
+		//если текущая строка состояла только из комментария, то запоминаем количество затираемых строк как разницу последней строки и начала затирания
+		(text[endY][0] == '\0') ? quantityStr = (endY)-beginStr + 1 : quantityStr = (endY)-beginStr;
+
+		//Удаляем строки от начального до конечного номера строки   
+		lengthOfText = deletingRows(text, lengthOfText, beginStr, quantityStr);
+
+	}
+
+	return lengthOfText;
 }
 
 
